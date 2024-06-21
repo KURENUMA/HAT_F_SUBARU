c422d64f767cfb7436ff1ee5106e85f856199258
小磯和昭 <kkoiso@di-system.co.jp>
2024-06-21 20:23:49 +0900

    アセンブリ0.2406.18.0におけるアセンブリカスタムモデル追加漏れ

---
+++ HAT_F_SUBARU/HAT_F_api/CustomModels/InternalDeliveryCheckParameter.cs
@@ -0,0 +1,15 @@
+﻿namespace HAT_F_api.CustomModels
+{
+    /// <summary>納品一覧表（社内用）チェック記録用パラメータ</summary>
+    public class InternalDeliveryCheckParameter
+    {
+        /// <summary>売上番号</summary>
+        public string SalesNo { get; set; }
+
+        /// <summary>売上行番号</summary>
+        public short RowNo { get; set; }
+
+        /// <summary>コメント</summary>
+        public string Comment { get; set; }
+    }
+}
---
+++ HAT_F_SUBARU/HAT_F_api/CustomModels/InternalDeliveryCheckResult.cs
@@ -0,0 +1,21 @@
+﻿namespace HAT_F_api.CustomModels
+{
+    /// <summary>納品一覧表（社内用）チェック結果</summary>
+    public class InternalDeliveryCheckResult
+    {
+        /// <summary>売上番号</summary>
+        public string SalesNo { get; set; }
+
+        /// <summary>売上行番号</summary>
+        public short RowNo { get; set; }
+
+        /// <summary>チェック者名</summary>
+        public string Checker { get; set; }
+
+        /// <summary>チェック者役職</summary>
+        public string CheckerPost { get; set; }
+
+        /// <summary>コメント</summary>
+        public string Comment { get; set; }
+    }
+}
